88200e8cc7cc2ae6b560716ee4a2185e22c9c9ae
nick1227 <lang_zheng27@hotmail.com>
2016-07-19 18:45:33 +0800

    Basic view now returns minimal HTML

--- functional_tests.py
+++ functional_tests.py
@@ -1,10 +1,21 @@
 
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-
-assert 'To-Do' in browser.title
-
-
-browser.qiut()
+class NewVisitorTest(unittest.TestCase):
+	
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
+		
+	def tearDown(self):
+		self.browser.quit()
+	
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		self.browser.get('http://localhost:8000')
+		#self.browser.get('http://192.168.136.6:8088/ws/')
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
+		
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
