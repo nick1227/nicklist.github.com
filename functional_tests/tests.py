b1fb3dae155db88ca35d7ae63875571b3f1c095b
nick1227 <lang_zheng27@hotmail.com>
2016-07-23 12:13:31 +0800

    Use Bootstrap to imporve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,11 @@
 
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 	
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -31,7 +32,7 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 		
-		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys(' Buy peacock feathers')
 		
 		inputbox.send_keys(Keys.ENTER)
 		#self.check_for_row_in_list_table('1: Buy peacock feathers')
@@ -41,7 +42,7 @@ class NewVisitorTest(LiveServerTestCase):
 
 		# 2
 		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Use peacock feathers to make a fly')	
+		inputbox.send_keys(' Use peacock feathers to make a fly')	
 		inputbox.send_keys(Keys.ENTER)
 		
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
@@ -75,3 +76,10 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertIn('Buy milk', page_text)
 		
 		self.fail('Finish the test!')
+
+	def test_layout_and_styling(self):
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024,768)
+		
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2, 512,delta=5)
